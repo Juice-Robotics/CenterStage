7174dd06c4c37c44fe150ec53deb231502d333a9
mongbean <zhimingqi9@gmail.com>
2023-09-21 22:16:00 -0700

    different deposit functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
@@ -10,15 +10,28 @@ public class Deposit {
     //presets (????????????????????)
     public double open = 1;
     public double close = 0;
+    public double epsilon = 0.1;
 
     public Deposit(Servo depositServo) {
         this.depositServo = depositServo;
         currentAngle = depositServo.getPosition();
     }
-    public void turnToPreset(double preset){
-        depositServo.setPosition(preset);
+    public void open(){
+        depositServo.setPosition(open);
+        currentAngle = open;
     }
-
+    public void close(){
+        depositServo.setPosition(close);
+        currentAngle = close;
+    }
+    public void toggle(){
+        if (currentAngle == open){
+            close();
+        }
+        else{
+            open();
+        }
+    }
     public void depositToPos(double pos){
         depositServo.setPosition(pos);
     }
