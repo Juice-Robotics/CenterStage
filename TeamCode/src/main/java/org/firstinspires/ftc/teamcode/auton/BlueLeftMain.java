9b6ced4773df2af11c2a9fe04bd8547998b0ebcb
Siddharth Ray <sidraystudios@gmail.com>
2023-10-06 22:08:53 -0700

    added the 50  path variations to auton im a centerstage hater now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueLeftMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/BlueLeftMain.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.auton;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
@@ -10,6 +11,7 @@ import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.lib.AllianceColor;
 import org.firstinspires.ftc.teamcode.subsystems.vision.TeamElementCVProcessor;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 
 import java.util.ArrayList;
@@ -39,6 +41,78 @@ public class BlueLeftMain extends LinearOpMode {
         visionPortal = new VisionPortal.Builder()
                 .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1")) // the camera on your robot is named "Webcam 1" by default
                 .addProcessor(teamElementProcessor)
+                .build();
+
+        // PRELOAD PATHS
+        TrajectorySequence preloadSpikeLeft = drive.trajectorySequenceBuilder(startPose)
+                .splineTo(new Vector2d(-45, 20), Math.toRadians(15))
+                .strafeLeft(10)
+                .build();
+        TrajectorySequence preloadBackdropLeft = drive.trajectorySequenceBuilder(preloadSpikeLeft.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-29, 49), Math.toRadians(90))
+                .build();
+        TrajectorySequence leftToStack1 = drive.trajectorySequenceBuilder(preloadBackdropLeft.end())
+                .setReversed(true)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                .build();
+
+        TrajectorySequence preloadSpikeCenter = drive.trajectorySequenceBuilder(startPose)
+                .forward(20)
+                .back(10)
+                .build();
+        TrajectorySequence preloadBackdropCenter = drive.trajectorySequenceBuilder(preloadSpikeCenter.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+                .build();
+        TrajectorySequence centerToStack1 = drive.trajectorySequenceBuilder(preloadBackdropCenter.end())
+                .setReversed(true)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                .build();
+
+        TrajectorySequence preloadSpikeRight = drive.trajectorySequenceBuilder(startPose)
+                .splineTo(new Vector2d(-43, 8), Math.toRadians(-15))
+                .back(10)
+                .build();
+        TrajectorySequence preloadBackdropRight = drive.trajectorySequenceBuilder(preloadSpikeRight.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-40, 49), Math.toRadians(90))
+                .build();
+        TrajectorySequence rightToStack1 = drive.trajectorySequenceBuilder(preloadBackdropRight.end())
+                .setReversed(true)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                .build();
+
+        // SHARED PATHS
+        TrajectorySequence stackToBackdrop1 = drive.trajectorySequenceBuilder(leftToStack1.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+                .build();
+
+        TrajectorySequence backdropToStack2 = drive.trajectorySequenceBuilder(stackToBackdrop1.end())
+                .setReversed(true)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                .build();
+        TrajectorySequence stackToBackdrop2 = drive.trajectorySequenceBuilder(backdropToStack2.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+                .build();
+
+        TrajectorySequence backdropToStack3 = drive.trajectorySequenceBuilder(stackToBackdrop2.end())
+                .setReversed(true)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                .build();
+        TrajectorySequence stackToBackdrop3 = drive.trajectorySequenceBuilder(backdropToStack3.end())
+                .setReversed(false)
+                .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
                 .build();
 
         /*
@@ -80,6 +154,32 @@ public class BlueLeftMain extends LinearOpMode {
         waitForStart();
 
         if (isStopRequested()) return;
+
+        switch (propLocation) {
+            case CENTER:
+                drive.followTrajectorySequence(preloadSpikeCenter);
+                drive.followTrajectorySequence(preloadBackdropCenter);
+                drive.followTrajectorySequence(centerToStack1);
+                break;
+            case LEFT:
+                drive.followTrajectorySequence(preloadSpikeLeft);
+                drive.followTrajectorySequence(preloadBackdropLeft);
+                drive.followTrajectorySequence(leftToStack1);
+                break;
+            case RIGHT:
+                drive.followTrajectorySequence(preloadSpikeRight);
+                drive.followTrajectorySequence(preloadBackdropCenter);
+                drive.followTrajectorySequence(rightToStack1);
+                break;
+        }
+
+        drive.followTrajectorySequence(stackToBackdrop1);
+        drive.followTrajectorySequence(backdropToStack2);
+        drive.followTrajectorySequence(stackToBackdrop2);
+        drive.followTrajectorySequence(backdropToStack3);
+        drive.followTrajectorySequence(stackToBackdrop3);
+
+        while (!isStopRequested() && opModeIsActive()) ;
     }
 
     public static double rad(double degrees) {
