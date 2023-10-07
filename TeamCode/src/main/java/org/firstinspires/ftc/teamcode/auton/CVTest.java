1978393e5b3f7934980e6c86fe1c8572c4c99ff9
Siddharth Ray <sidraystudios@gmail.com>
2023-10-06 17:28:53 -0700

    putting some glasses on the bot so it can see

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/CVTest.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.auton;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.lib.AllianceColor;
+import org.firstinspires.ftc.teamcode.subsystems.vision.TeamElementCVProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Disabled // remove this line to have this show up on your robot
+@Autonomous
+public class CVTest extends OpMode {
+    private VisionPortal visionPortal;
+    private TeamElementCVProcessor teamElementProcessor;
+
+
+    @Override
+    public void init() {
+        teamElementProcessor = new TeamElementCVProcessor(
+                () -> 100, // these are lambda methods, in case we want to change them while the match is running, for us to tune them or something
+                () -> 213, // the left dividing line, in this case the left third of the frame
+                () -> 426, // the left dividing line, in this case the right third of the frame,
+                telemetry,
+                AllianceColor.BLUE);
+        visionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1")) // the camera on your robot is named "Webcam 1" by default
+                .addProcessor(teamElementProcessor)
+                .build();
+    }
+
+
+    @Override
+    public void init_loop() {
+        telemetry.addData("Camera State", visionPortal.getCameraState());
+    }
+
+
+    @Override
+    public void start() {
+        // shuts down the camera once the match starts, we dont need to look any more
+        if (visionPortal.getCameraState() == VisionPortal.CameraState.STREAMING) {
+            visionPortal.stopLiveView();
+            visionPortal.stopStreaming();
+        }
+
+        // gets the recorded prop position
+        TeamElementCVProcessor.Location recordedPropPosition = teamElementProcessor.getLocation();
+
+        // if it is UNFOUND, you can manually set it to any of the other positions to guess
+        if (recordedPropPosition == TeamElementCVProcessor.Location.UNFOUND) {
+            recordedPropPosition = TeamElementCVProcessor.Location.CENTER;
+        }
+
+
+        // now we can use recordedPropPosition in our auto code to modify where we place the purple and yellow pixels
+        switch (recordedPropPosition) {
+            case LEFT:
+                telemetry.addData("running", "left spike auton");
+                break;
+            case UNFOUND: // we can also just add the unfound case here to do fallthrough intstead of the overriding method above, whatever you prefer!
+                telemetry.addLine("warning: <b>unfound spike auton</b>");
+                break;
+            case CENTER:
+                telemetry.addData("running", "center spike auton");
+                break;
+            case RIGHT:
+                telemetry.addData("running", "right spike auton");
+                break;
+        }
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void stop() {
+        // this closes down the portal when we stop the code, its good practice!
+        teamElementProcessor.close();
+        visionPortal.close();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/vision/TeamElementCVProcessor.java
@@ -0,0 +1,256 @@
+package org.firstinspires.ftc.teamcode.subsystems.vision;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.text.TextPaint;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.teamcode.lib.AllianceColor;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.function.DoubleSupplier;
+
+public class TeamElementCVProcessor implements VisionProcessor {
+    private final DoubleSupplier minArea, left, right;
+    private final Scalar upper; // lower bounds for masking
+    private final Scalar lower; // upper bounds for masking
+
+    private final TextPaint textPaint;
+    private final Paint linePaint;
+
+    private final ArrayList<MatOfPoint> contours;
+
+    private final Mat hierarchy = new Mat();
+
+    private double largestContourX;
+    private double largestContourY;
+    private double largestContourArea;
+    private MatOfPoint largestContour;
+
+    private Location previousPropPosition;
+    private Location recordedPropPosition = Location.UNFOUND;
+
+    AllianceColor alliance;
+    Telemetry telemetry;
+
+    /**
+     * Uses HSVs for the scalars
+     *
+     * @param lower   the lower masked bound, a three a value scalar in the form of a HSV
+     * @param upper   the upper masked bound, a three a value scalar in the form of a HSV
+     * @param minArea the minimum area for a detected blob to be considered the prop
+     * @param left    the dividing point for the prop to be on the left
+     * @param right   the diving point for the prop to be on the right
+     */
+    public TeamElementCVProcessor(DoubleSupplier minArea, DoubleSupplier left, DoubleSupplier right, Telemetry t, AllianceColor a) {
+
+        Scalar lower;
+        Scalar upper;
+        alliance = a;
+        telemetry = t;
+
+        if (alliance == AllianceColor.BLUE) {
+            lower = new Scalar(150, 100, 100);
+            upper = new Scalar(180, 255, 255);
+        } else {
+            upper = new Scalar(0, 0, 255);
+            lower = new Scalar(0, 255, 0);
+        }
+
+        this.contours = new ArrayList<>();
+        this.lower = lower;
+        this.upper = upper;
+        this.minArea = minArea;
+        this.left = left;
+        this.right = right;
+
+        // setting up the paint for the text in the center of the box
+        textPaint = new TextPaint();
+        textPaint.setColor(Color.GREEN); // you may want to change this
+        textPaint.setTextAlign(Paint.Align.CENTER);
+        textPaint.setAntiAlias(true);
+        textPaint.setTextSize(40); // or this
+        textPaint.setTypeface(Typeface.DEFAULT_BOLD);
+
+        // setting up the paint for the lines that comprise the box
+        linePaint = new Paint();
+        linePaint.setColor(Color.GREEN); // you may want to change this
+        linePaint.setAntiAlias(true);
+        linePaint.setStrokeWidth(10); // or this
+        linePaint.setStrokeCap(Paint.Cap.ROUND);
+        linePaint.setStrokeJoin(Paint.Join.ROUND);
+    }
+
+    @Override
+    public void init(int width, int height, CameraCalibration calibration) {
+        // this method comes with all VisionProcessors, we just don't need to do anything here, and you dont need to call it
+    }
+
+    /**
+     * @return the x position of the currently found largest contour in the range [0, camera width], or -1 if no largest contour has been determined
+     */
+    public double getLargestContourX() {
+        return largestContourX;
+    }
+
+    /**
+     * @return the y position of the currently found largest contour in the range [0, camera height], or -1 if no largest contour has been determined
+     */
+    public double getLargestContourY() {
+        return largestContourY;
+    }
+
+    /**
+     * @return the area of the currently found largest contour, or -1 if no largest contour has been determined
+     */
+    public double getLargestContourArea() {
+        return largestContourArea;
+    }
+
+    @Override
+    public Object processFrame(Mat frame, long captureTimeNanos) {
+        // this method processes the image (frame) taken by the camera, and tries to find a suitable prop
+        // you dont need to call it
+
+        // this converts the frame from RGB to HSV, which is supposed to be better for doing colour blob detection
+        Imgproc.cvtColor(frame, frame, Imgproc.COLOR_RGB2HSV);
+        // thats why you need to give your scalar upper and lower bounds as HSV values
+
+        // this method makes the colour image black and white, with everything between your upper and lower bound values as white, and everything else black
+        Core.inRange(frame, lower, upper, frame);
+
+        // this empties out the list of found contours, otherwise we would keep all the old ones, read on to find out more about contours!
+        contours.clear();
+
+        // this finds the contours, which are borders between black and white, and tries to simplify them to make nice outlines around potential objects
+        // this basically helps us to find all the shapes/outlines of objects that exist within our colour range
+        Imgproc.findContours(frame, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // this sets up our largest contour area to be 0
+        largestContourArea = -1;
+        // and our currently found largest contour to be null
+        largestContour = null;
+
+        // gets the current minimum area from min area
+        double minArea = this.minArea.getAsDouble();
+
+        // finds the largest contour!
+        // for each contour we found before we loop over them, calculate their area,
+        // and then if our area is larger than our minimum area, and our currently found largest area
+        // it stores the contour as our largest contour and the area as our largest area
+        for (MatOfPoint contour : contours) {
+            double area = Imgproc.contourArea(contour);
+            if (area > largestContourArea && area > minArea) {
+                largestContour = contour;
+                largestContourArea = area;
+            }
+        }
+
+        // sets up the center points of our largest contour to be -1 (offscreen)
+        largestContourX = largestContourY = -1;
+
+        // if we found it, calculates the actual centers
+        if (largestContour != null) {
+            Moments moment = Imgproc.moments(largestContour);
+            largestContourX = (moment.m10 / moment.m00);
+            largestContourY = (moment.m01 / moment.m00);
+        }
+
+        // determines the current prop position, using the left and right dividers we gave earlier
+        // if we didn't find any contours which were large enough, sets it to be unfound
+        Location propPosition;
+        if (largestContour == null) {
+            propPosition = Location.UNFOUND;
+            telemetry.addLine("Team Element Location: <b>NOT FOUND</b>");
+        } else if (largestContourX < left.getAsDouble()) {
+            propPosition = Location.LEFT;
+            telemetry.addData("Team Element Location", "left");
+        } else if (largestContourX > right.getAsDouble()) {
+            propPosition = Location.RIGHT;
+            telemetry.addData("Team Element Location", "right");
+        } else {
+            propPosition = Location.CENTER;
+            telemetry.addData("Team Element Location", "center");
+        }
+        telemetry.update();
+
+        // if we have found a new prop position, and it is not unfound, updates the recorded position,
+        // this makes sure that if our camera is playing up, we only need to see the prop in the correct position
+        // and we will hold onto it
+        if (propPosition != previousPropPosition && propPosition != Location.UNFOUND) {
+            recordedPropPosition = propPosition;
+        }
+
+        // updates the previous prop position to help us check for changes
+        previousPropPosition = propPosition;
+
+//		Imgproc.drawContours(frame, contours, -1, colour);
+
+        // returns back the edited image, don't worry about this too much
+        return frame;
+    }
+
+    @Override
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+        // this method draws the rectangle around the largest contour and puts the current prop position into that rectangle
+        // you don't need to call it
+
+//		for (MatOfPoint contour : contours) {
+//			Rect rect = Imgproc.boundingRect(contour);
+//			canvas.drawLines(new float[]{rect.x * scaleBmpPxToCanvasPx, rect.y * scaleBmpPxToCanvasPx, (rect.x + rect.width) * scaleBmpPxToCanvasPx, (rect.y + rect.height) * scaleBmpPxToCanvasPx}, textPaint);
+//		}
+
+        // if the contour exists, draw a rectangle around it and put its position in the middle of the rectangle
+        if (largestContour != null) {
+            Rect rect = Imgproc.boundingRect(largestContour);
+
+            float[] points = {rect.x * scaleBmpPxToCanvasPx, rect.y * scaleBmpPxToCanvasPx, (rect.x + rect.width) * scaleBmpPxToCanvasPx, (rect.y + rect.height) * scaleBmpPxToCanvasPx};
+
+            canvas.drawLine(points[0], points[1], points[0], points[3], linePaint);
+            canvas.drawLine(points[0], points[1], points[2], points[1], linePaint);
+
+            canvas.drawLine(points[0], points[3], points[2], points[3], linePaint);
+            canvas.drawLine(points[2], points[1], points[2], points[3], linePaint);
+
+            String text = String.format(Locale.ENGLISH, "%s", recordedPropPosition.toString());
+
+            canvas.drawText(text, (float) largestContourX * scaleBmpPxToCanvasPx, (float) largestContourY * scaleBmpPxToCanvasPx, textPaint);
+        }
+    }
+
+    /**
+     * @return the last found prop position, if none have been found, returns {@link Location#UNFOUND}
+     */
+    public Location getLocation() {
+        return recordedPropPosition;
+    }
+
+    // returns the largest contour if you want to get information about it
+    public MatOfPoint getLargestContour() {
+        return largestContour;
+    }
+
+    public void close() {
+        hierarchy.release();
+    }
+
+    // the enum that stores the 4 possible prop positions
+    public enum Location {
+        LEFT,
+        CENTER,
+        RIGHT,
+        UNFOUND;
+    }
+}
