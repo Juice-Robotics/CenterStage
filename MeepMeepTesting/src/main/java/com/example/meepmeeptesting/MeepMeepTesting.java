397aa8f1eedf26001a659ce334142a03caf759c2
Siddharth Ray <sidraystudios@gmail.com>
2023-10-29 11:36:00 -0700

    other side autos for blue

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -150,6 +150,80 @@ public class MeepMeepTesting {
                                         .build()
                 );
 
+        RoadRunnerBotEntity myBotCyclesUnsafeSide = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+//                .setColorScheme(new ColorSchemeRedDark())
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setDimensions(18, 18)
+                .followTrajectorySequence(drive ->
+                                drive.trajectorySequenceBuilder(new Pose2d(-59, -35, 0))
+                                        //CENTER
+//                                        .forward(25)
+//                                        .back(10)
+//                                        .splineTo(new Vector2d(-35, -60), Math.toRadians(270))
+//                                        .setReversed(false)
+//                                        .splineTo(new Vector2d(-10, -42.5), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-29, 49), Math.toRadians(90))
+
+                                        // RIGHT
+//                                        .splineTo(new Vector2d(-43, -42), Math.toRadians(-15))
+//                                        .back(10)
+//                                        .setReversed(false)
+//                                        .lineToSplineHeading(new Pose2d(-10, -31, Math.toRadians(90)))
+//                                        .setReversed(true)
+//                                        .splineTo(new Vector2d(-11, -60), Math.toRadians(270))
+//                                        .setReversed(false)
+//                                        .splineTo(new Vector2d(-10, -42.5), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-29, 49), Math.toRadians(90))
+
+                                        //RIGHT sussy
+                                        .forward(26)
+                                        .turn(Math.toRadians(90))
+                                        .strafeRight(21)
+                                        .turn(Math.toRadians(180))
+                                        .forward(20)
+//                                        .back(40)
+                                        .setReversed(true)
+                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                                        .splineTo(new Vector2d(-29, 49), Math.toRadians(90))
+
+                                        //LEFT
+//                                        .splineTo(new Vector2d(-36, -34), Math.toRadians(55))
+//                                        .setReversed(true)
+//                                        .splineTo(new Vector2d(-35, -60), Math.toRadians(270))
+//                                        .setReversed(false)
+//                                        .splineTo(new Vector2d(-10, -42.5), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-29, 49), Math.toRadians(90))
+
+                                        .setReversed(false)
+                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                                        .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                                        .setReversed(true)
+                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                                        .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+
+                                        .setReversed(false)
+                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+                                        .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+                                        .setReversed(true)
+                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+                                        .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+
+//                                        .setReversed(false)
+//                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(-90))
+//                                        .splineTo(new Vector2d(-11, -61), Math.toRadians(-90))
+//                                        .setReversed(true)
+//                                        .splineTo(new Vector2d(-10, 20), Math.toRadians(90))
+//                                        .splineTo(new Vector2d(-36, 49), Math.toRadians(90))
+
+
+
+                                        .build()
+                );
+
         Image img = null;
         try { img = ImageIO.read(new File("/Users/siddharth/dev/Juice/CenterStage/MeepMeepTesting/src/main/java/com/example/meepmeeptesting/Juice-CENTERSTAGE-Dark.png")); }
         catch (IOException e) {}
@@ -158,8 +232,8 @@ public class MeepMeepTesting {
 //        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBotCyclesSafe)
-                .addEntity(myBotCyclesSafeRed)
+                .addEntity(myBotCyclesUnsafeSide)
+//                .addEntity(myBotCyclesSafeRed)
                 .start();
     }
 }
