53e85d4a9461b334fe867936ee3db8001fdceb12
Siddharth Ray <sidraystudios@gmail.com>
2024-01-13 21:11:04 -0800

    toonin

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedBackdropSidePreload.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/RedBackdropSidePreload.java
@@ -30,7 +30,7 @@ public class RedBackdropSidePreload extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Scalar lower = new Scalar(150, 100, 100); // the lower hsv threshold for your detection
+        Scalar lower = new Scalar(150, 70, 100); // the lower hsv threshold for your detection
         Scalar upper = new Scalar(180, 255, 255); // the upper hsv threshold for your detection
         double minArea = 100; // the minimum area for the detection to consider for your prop
 
